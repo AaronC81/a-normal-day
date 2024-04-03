3bab70393db1c9afd58ae65ba97c8f5f75398631
Aaron Christiansen <aaronc20000@gmail.com>
2024-04-03 22:36:35 +0100

    Finally constraint character movement properly

--- src/entity/character.rb
+++ src/entity/character.rb
@@ -25,8 +25,21 @@ module GosuGameJam6
         end
 
         def valid_position?
-            !Game::WALLS.items.any? { |wall| wall.bounding_box.overlaps?(bounding_box) } \
-                && Game::OPEN_AREAS.items.any? { |area| area.bounding_box.overlaps?(bounding_box) }
+            # All four corners of the character must be touching an open area
+            # (Doesn't need to be the same one, though)
+            bb = bounding_box
+            points = [
+                position,
+                position + OZ::Point[bb.width, 0],
+                position + OZ::Point[0, bb.height],
+                position + OZ::Point[bb.width, bb.height],
+            ]
+            area_check = points.all? { |pt| Game::OPEN_AREAS.items.any? { |area| area.bounding_box.point_inside?(pt) } }
+
+            # Can't be inside a wall
+            wall_check = !Game::WALLS.items.any? { |wall| wall.bounding_box.overlaps?(bounding_box) } \
+
+            area_check && wall_check
         end
     end
 end
